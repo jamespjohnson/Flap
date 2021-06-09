406de183649dbc9b181970156bdae566e1df7672
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-06-09 13:52:03 -0700

    test

---
+++ Flap/Assets/_Folders/Scripts/death.cs
@@ -0,0 +1,27 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class death : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     void OnTriggerEnter2D(Collider2D other)
+//     {
+//         if(other.gameObject.tag == "Player")
+//         {
+
+//         }
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         GameObject topB = GameObject.Find("top");
+//         GameObject bottomB = GameObject.Find("bottom");
+//         if(transform.localPosition.y == transform.topB.y || transform.localPosition == transform.bottomB.y)
+//         {
+//             kill();
+//         }
+        
+//     }
+// }
