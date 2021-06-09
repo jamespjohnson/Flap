db86f4262ab27c5d2498425ecf63e4535d394c69
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-06-09 16:01:08 -0700

    death

---
+++ Flap/Assets/_Folders/Scripts/PlayerDire.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Pipe"))
+            print("Player died");
+        else
+            print("poop");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
