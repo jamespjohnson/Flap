ba9547f3582ff9d29cd3c57fc3e52c171d06a40e
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-06-10 08:59:21 -0700

    3d

--- Flap/Assets/_Folders/Scripts/HorizontalTween.cs
+++ Flap/Assets/_Folders/Scripts/HorizontalTween.cs
@@ -9,14 +9,14 @@ public class HorizontalTween : MonoBehaviour
     void GoBack()
     {
         Vector3 pos = transform.localPosition;
-        pos.x += 24;
+        pos.x += 33;
         pos.y = Random.Range(1.5f, 6.5f);
         transform.localPosition = pos;
-        transform.DOMoveX(transform.localPosition.x - 24, 1.5f).SetEase(Ease.Linear).OnComplete(GoBack).SetAutoKill(true);
+        transform.DOMoveX(transform.localPosition.x - 33, 1.5f).SetEase(Ease.Linear).OnComplete(GoBack).SetAutoKill(true);
     }
 
     void Start()
     {
-        transform.DOMoveX(transform.localPosition.x - 24, 1.5f).SetEase(Ease.Linear).OnComplete(GoBack).SetAutoKill(true);
+        transform.DOMoveX(transform.localPosition.x - 33, 1.6f).SetEase(Ease.Linear).OnComplete(GoBack).SetAutoKill(true);
     }
 }
