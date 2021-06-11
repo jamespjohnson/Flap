c6a6f3c84e3cd7aafe0691795c181598d6f4f611
jamespjohnson <45546400+jamespjohnson@users.noreply.github.com>
2021-06-10 20:49:55 -0700

    update

--- Flap/Assets/_Folders/Scripts/PlayerDire.cs
+++ Flap/Assets/_Folders/Scripts/PlayerDire.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDire : MonoBehaviour
 {
@@ -13,7 +14,15 @@ public class PlayerDire : MonoBehaviour
     void OnTriggerEnter(Collider col)
     {
         if(col.CompareTag("Pipe"))
+<<<<<<< Updated upstream
             print("Player died");
+=======
+        {
+            TextMesh t = GameObject.Find("ScoreText").GetComponent<TextMesh>();
+            t.text = "0";
+            SceneManager.LoadScene(0);
+        }
+>>>>>>> Stashed changes
     }
     // Update is called once per frame
     void Update()
